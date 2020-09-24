63b3b8cb02a8a5237e0cc30f8a93f2d27f7d8743
Aleksandr Zubakin <eldoson@yandex.ru>
2020-09-24 21:46:32 +0500

    first 2 functions from previus rep of basic js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+  const cat = '^^';
+  let count = 0;
+  const myArray = Array.from(arguments[0]);
+  if (myArray.length === 0) return count;
+  for (let i = 0; i < myArray.length; i++) {
+    for (let j = 0; j < myArray[i].length; j++) {
+      if (myArray[i][j] === cat) count++;
+    }
+  }
+  return count;
   // remove line with error and write your code here
 };
+
+//console.log(module.exports([[1, 34], [5, 'dfgdf,'], ['^^']]));
