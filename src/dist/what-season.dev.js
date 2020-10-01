bde0b3be48e2fa9d9946dbf8c8a9ab5e61b14682
Aleksandr Zubakin <eldoson@yandex.ru>
2020-10-01 20:57:14 +0500

    feat: what season task

--- src/dist/what-season.dev.js
+++ src/dist/what-season.dev.js
@@ -1,14 +1,17 @@
-const CustomError = require("../extensions/custom-error");
+"use strict";
 
-module.exports = function getSeason( /* date */ ) {
+var CustomError = require("../extensions/custom-error");
+
+module.exports = function getSeason()
+/* date */
+{
   if (arguments[0] == undefined) return 'Unable to determine the time of year!';
-  const date = arguments[0];
+  var date = arguments[0];
   if (Object.prototype.toString.call(date) != '[object Date]') throw Error;
-  const month = date.getMonth();
+  var month = date.getMonth();
   if (month === 11 || month < 2) return 'winter';
   if (month > 1 && month < 5) return 'spring';
-  if (month > 4 && month < 8) return 'summer';
-  else return 'autumn';
+  if (month > 4 && month < 8) return 'summer';else return 'autumn';
 };
 
 console.log(module.exports(new Date(1994, 1, 2, 3, 4, 5)));
