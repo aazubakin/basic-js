05190cd745ddc1e1505d82ced008219be7730c7a
Aleksandr Zubakin <eldoson@yandex.ru>
2020-10-02 20:07:04 +0500

    feat: add solution tower of Hanoi

--- src/dist/hanoi-tower.dev.js
+++ src/dist/hanoi-tower.dev.js
@@ -1,4 +1,6 @@
-const CustomError = require("../extensions/custom-error");
+"use strict";
+
+var CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   numTransfers = Math.pow(2, disksNumber) - 1;
@@ -7,6 +9,4 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     turns: numTransfers,
     seconds: timeToTransfer
   };
-};
-
-//console.log(module.exports(5, 4074));
+}; //console.log(module.exports(5, 4074));
