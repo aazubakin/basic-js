11868647af200094b018f54f80a0005ef2ceafdb
Aleksandr Zubakin <eldoson@yandex.ru>
2020-10-06 20:11:07 +0500

    feat: add solution of extended repetition

--- src/dist/extended-repeater.dev.js
+++ src/dist/extended-repeater.dev.js
@@ -9,12 +9,12 @@ module.exports = function repeater(str, options) {
   if (myObj.additionSeparator === undefined) myObj.additionSeparator = '|';
   if (myObj.repeatTimes === undefined) myObj.repeatTimes = 1;
   if (myObj.addition === undefined) myObj.addition = '';
-  if (myObj.additionRepeatTimes === undefined) myObj.additionRepeatTimes = 0;
+  if (myObj.additionRepeatTimes === undefined) myObj.additionRepeatTimes = 1;
   myObj.str = str;
-  myObj.additionStr = '';
+  myObj.additionStr = myObj.addition; //make addition string
 
-  for (var j = 0; j < myObj.additionRepeatTimes; j++) {
-    myObj.additionStr += myObj.addition;
+  for (var j = 0; j < myObj.additionRepeatTimes - 1; j++) {
+    if (myObj.additionRepeatTimes > 1) myObj.additionStr += myObj.additionSeparator + myObj.addition; //else myObj.additionStr += myObj.addition;
   }
 
   myObj.str += myObj.additionStr;
@@ -28,13 +28,31 @@ module.exports = function repeater(str, options) {
 
   return myObj.str;
 };
-
+/*
 console.log(module.exports('la', {
   repeatTimes: 3
-}));
+})); //'la+la+la'
 console.log(module.exports('la', {
   repeatTimes: 3,
   separator: 's',
   addition: '+',
   additionRepeatTimes: 1
-}));
+})); //'la+sla+sla+'
+
+console.log(module.exports('TESTstr', {
+  repeatTimes: undefined,
+  separator: 'ds',
+  addition: 'ADD!',
+  additionRepeatTimes: undefined,
+  additionSeparator: ')))000'
+})); //'TESTstrADD!'
+
+console.log(module.exports('my test string', {
+  repeatTimes: 5,
+  separator: '?!',
+  addition: 'PLUS',
+  additionRepeatTimes: 4,
+  additionSeparator: '))'
+}) === 'my test stringPLUS))PLUS))PLUS))PLUS?!my test stringPLUS))PLUS))PLUS))PLUS?!my test stringPLUS))PLUS))PLUS))PLUS?!my test stringPLUS))PLUS))PLUS))PLUS?!my test stringPLUS))PLUS))PLUS))PLUS');
+
+*/
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -7,30 +7,52 @@ module.exports = function repeater(str, options) {
   if (myObj.additionSeparator === undefined) myObj.additionSeparator = '|';
   if (myObj.repeatTimes === undefined) myObj.repeatTimes = 1;
   if (myObj.addition === undefined) myObj.addition = '';
-  if (myObj.additionRepeatTimes === undefined) myObj.additionRepeatTimes = 0;
+  if (myObj.additionRepeatTimes === undefined) myObj.additionRepeatTimes = 1;
   myObj.str = str;
-  myObj.additionStr = '';
+  myObj.additionStr = myObj.addition;
 
-  for (let j = 0; j < myObj.additionRepeatTimes; j++) {
-    myObj.additionStr += myObj.addition;
+  //make addition string
+  for (let j = 0; j < myObj.additionRepeatTimes - 1; j++) {
+    if (myObj.additionRepeatTimes > 1) myObj.additionStr += myObj.additionSeparator + myObj.addition;
+    //else myObj.additionStr += myObj.addition;
   }
+
   myObj.str += myObj.additionStr;
   str = myObj.str;
 
   if (myObj.repeatTimes !== 1) {
     for (let i = 0; i < myObj.repeatTimes - 1; i++) {
       myObj.str = myObj.str + myObj.separator + str;
-
     }
   }
   return myObj.str;
 };
+
+/*
 console.log(module.exports('la', {
   repeatTimes: 3
-}));
+})); //'la+la+la'
 console.log(module.exports('la', {
   repeatTimes: 3,
   separator: 's',
   addition: '+',
   additionRepeatTimes: 1
-}));
+})); //'la+sla+sla+'
+
+console.log(module.exports('TESTstr', {
+  repeatTimes: undefined,
+  separator: 'ds',
+  addition: 'ADD!',
+  additionRepeatTimes: undefined,
+  additionSeparator: ')))000'
+})); //'TESTstrADD!'
+
+console.log(module.exports('my test string', {
+  repeatTimes: 5,
+  separator: '?!',
+  addition: 'PLUS',
+  additionRepeatTimes: 4,
+  additionSeparator: '))'
+}) === 'my test stringPLUS))PLUS))PLUS))PLUS?!my test stringPLUS))PLUS))PLUS))PLUS?!my test stringPLUS))PLUS))PLUS))PLUS?!my test stringPLUS))PLUS))PLUS))PLUS?!my test stringPLUS))PLUS))PLUS))PLUS');
+
+*/
